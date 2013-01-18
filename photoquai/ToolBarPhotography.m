3e51fce81d18e5cdd7a7ef3d3397e810bd94780b
Dan42K <jeanlouis.danielo@sfr.fr>
2013-01-18 15:17:47 +0100

    Finition Toolbar

---
+++ photoquai/ToolBarPhotography.m
@@ -0,0 +1,31 @@
+//
+//  ToolBarPhotography.m
+//  photoquai
+//
+//  Created by Jean-Louis Danielo on 18/01/13.
+//  Copyright (c) 2013 Groupe 5 PHQ Gobelins CDNL. All rights reserved.
+//
+
+#import "ToolBarPhotography.h"
+
+@implementation ToolBarPhotography
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
