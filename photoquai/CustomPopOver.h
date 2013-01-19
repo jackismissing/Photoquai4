7cbc9524e4c329237338ed4c5159bd1e211fb4cf
Dan42K <jeanlouis.danielo@sfr.fr>
2013-01-19 22:56:27 +0100

    Ajout options de partage, début d'implémentation

---
+++ photoquai/CustomPopOver.h
@@ -0,0 +1,13 @@
+//
+//  CustomPopOver.h
+//  photoquai
+//
+//  Created by Jean-Louis Danielo on 19/01/13.
+//  Copyright (c) 2013 Groupe 5 PHQ Gobelins CDNL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomPopOver : UIView
+
+@end
